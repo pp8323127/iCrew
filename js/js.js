d9375c6f0e8ef6f7e991772db82579228e9f40a5
pp8323127 <pp8323127@gmail.com>
2015-06-08 15:08:57 +0800

    img update

--- js/js.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("#navbar").load("navbar.jsp"); 
-});
-// $(document).ready(function(){ 
-// $.get("navbar.html",function(data){ //初始將a.html include div#iframe
-// $("#navbar").html(data);
-// }); 
